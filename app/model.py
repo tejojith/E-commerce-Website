926b9b92c29596cb7a356565636c2cfbd015f81f
Soldiersolo24 <tejojith.g@gmail.com>
2024-06-18 00:52:53 +0530

    added the models

---
+++ app/model.py
@@ -0,0 +1,19 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String , Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, nullable= False)
+    title = Column(String, nullable = False )
+    content = Column(String, nullable = False)
+    # rating = Column(Integer, nullable = False)
+    published =  Column(Boolean, server_default  = 'True',nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable= False)
+    
+    owner = relationship("User")
---
+++ app/schemas.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+from pydantic.types import conint
+
+
+class Product(BaseModel):
+
+    id: int
+    name: str
+    img_link: str
+    price: int
+    stock: int 
+    size: str
+    gender: str
+    category: str
+
+    class Config:
+        orm_model = True
+
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    role: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    role: str
+    created_at: datetime
+    class Config:
+        orm_mode = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Cart(UserOut):
+    id: str
+
+
+
+
